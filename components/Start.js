f8f72d60e1b76f3cf1c53ed82857b08416acd9d9
a3laxi <mad.aalachi@gmail.com>
2023-04-01 16:14:21 +0200

    NGL CSS ON FIRE 💯💯💯

--- components/Start.js
+++ components/Start.js
@@ -1,61 +1,35 @@
-import styled from 'styled-components';
 import TwitterIcon from '@mui/icons-material/Twitter';
 import GitHubIcon from '@mui/icons-material/GitHub';
+import styles from '../styles/Start.module.css';
 
-const Container = styled.div`
-  margin-bottom: auto;
-  border: 5px solid black ;
-  width:70vw;
-  height:100%;
-  display: flex;
-`
-
-const BoxLeft = styled.div`
-  align-text: center;
-  border-right: 5px solid black;
-  display: inline-block;
-  padding: 10px 10px 10px 10px;
-`
-
-const BoxMid = styled.div`
-  align-text: center;
-  border-right: 5px solid black;
-  padding: 10px 10px 10px 10px;
-  display: flex;
-  width: 70%;
-  justify-content: center;
-  align-items: center;
-`
-
-const BoxRight = styled.div`
-  align-text: center;
-  padding: 10px 10px 10px 10px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  gap: 10px;
-`
 
 const Start = () => {
 
 
 
   return (
+    <>
+    <div className={styles.container} >
 
-    <Container>
-      <BoxLeft>
+      <div className={styles.boxleft} >
         <div>A3</div><div>LA</div>
-      </BoxLeft>
+      </div>
 
-      <BoxMid>
+      <div className={styles.boxmid} >
         <div>WHO AM I?</div>
-      </BoxMid>
-
-      <BoxRight>
+      </div>
+      
+      <div className={styles.boxright} >
         <TwitterIcon />
-        
-      </BoxRight>
-    </Container>
+        <GitHubIcon />
+      </div>
+
+    </div>
+    
+    </>
+
+
+   
   )
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 import Start from '../components/Start'
+
+
 export default function Home() {
 
 
@@ -12,34 +14,6 @@ export default function Home() {
       </Head>
 
       <Start />
-
-      
-
-      <style jsx>{`
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-        
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
     </div>
   )
 }
