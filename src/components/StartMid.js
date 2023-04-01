7d809469f87b28e6142a905583243eb055d053b3
a3laxi <mad.aalachi@gmail.com>
2023-04-01 20:08:12 +0200

    🤯🤯🤯

--- src/components/StartMid.js
+++ src/components/StartMid.js
@@ -10,7 +10,7 @@ const StartMid = () => {
       </div>
       <div className={styles.midheadright} >
         <div className={styles.midheadrighttop}>
-          Software Engineer passionate about solving problems related to <span style={{backgroundColor:'blue', color:'white'}} >Distributed Systems.</span>
+          <span style={{ fontSize:'25px' , fontWeight:'400'}} ><span style={{ fontSize:'30px' , fontWeight:'700' , color:'#f1f5f9'}} >CS Graduate</span> passionate about solving problems related to</span> <span style={{backgroundColor:'blue', color:'white'}} >Distributed Systems</span> <span style={{ fontSize:'25px' , fontWeight:'400'}} >and</span> <span style={{backgroundColor:'blue', color:'white'}} >Software</span>.
        
         </div>
         <div className={styles.midheadrightbottom}>
