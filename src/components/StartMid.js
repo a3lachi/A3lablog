81c1b9ea442accbdd967a35a98af48b7d875a442
a3laxi <mad.aalachi@gmail.com>
2023-04-01 19:11:14 +0200

    🚴​🚴​🚴​

--- src/components/StartMid.js
+++ src/components/StartMid.js
@@ -6,11 +6,16 @@ const StartMid = () => {
   return(
     <div  className={styles.midheadcontainer} >
       <div className={styles.midheadleft} >
-        <img width={580} src="/aloalo.png" />
+        <img width={680} src="/aloalo.png" />
       </div>
-      {/* <div className={styles.midheadright} >
-        
-      </div> */}
+      <div className={styles.midheadright} >
+        <div className={styles.midheadrighttop}>
+          I read palms, watch birds and write stable, maintainable and easily scalable code.
+        </div>
+        <div className={styles.midheadrightbottom}>
+          <div>- - - - - - - - - - - - - - - - - - - - - - - -  </div>
+        </div>
+      </div>
     </div>
 
   )
