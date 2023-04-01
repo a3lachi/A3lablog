4db994811f192e9c7f56bcfd97a26a65aad3ace8
a3laxi <mad.aalachi@gmail.com>
2023-04-01 22:10:33 +0200

    🤯🤯🤯

--- src/app/page.js
+++ src/app/page.js
@@ -8,7 +8,7 @@ export default function Home() {
     <main className={styles.main}>
       <Start />
       <Dots dotSize={23} width={600} top={210} left={-31} gap={30} color='#0369a1'  />
-      <Dots dotSize={13} width={600} top={410}  gap={20} right={50} color='#5eead4'/>
+      <Dots dotSize={13} width={600} top={404}  gap={20} right={50} color='#5eead4'/>
 
     </main>
   )
