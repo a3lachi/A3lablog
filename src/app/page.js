1b5691ec0e4f6f3fcd591ba83d91990d5610b114
a3laxi <mad.aalachi@gmail.com>
2023-04-01 18:09:58 +0200

    🚴​🚴​🚴​

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,10 @@
-import styles from './page.module.css'
-
+import styles from '../styles/page.module.css'
+import Start from '../components/Start'
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      
+      <Start />
     </main>
   )
 }
---
+++ src/components/Start.js
@@ -0,0 +1,35 @@
+import styles from '../styles/Start.module.css';
+
+
+const Start = () => {
+
+
+
+  return (
+    <>
+    <div className={styles.container} >
+
+      <div className={styles.boxleft} >
+        <div>A3</div><div>LA</div>
+      </div>
+
+      <div className={styles.boxmid}  >
+        <div>WHO AM I?</div>
+      </div>
+      
+      <div className={styles.boxright} >
+        H
+      </div>
+
+    </div>
+    
+    </>
+
+
+   
+  )
+}
+
+
+
+export default Start ;
