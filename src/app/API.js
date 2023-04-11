6cb817edc6fd132f9da01d9320e1216b56c6976a
a3laxi <mad.aalachi@gmail.com>
2023-04-11 15:50:42 +0200

    adding APIs

---
+++ src/app/API.js
@@ -0,0 +1,38 @@
+
+
+import styles from '../styles/Api.module.css'
+
+const Api = () => {
+
+
+  return(
+    <div className={styles.api} >
+      <div className={styles.apititle}>
+        APIs
+      </div>
+      <div  className={styles.list}>
+        <div className={styles.imgg}>
+          <img width={80} src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/75/Django_logo.svg/240px-Django_logo.svg.png" />
+          <img width={120} src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9f/Selenium_logo.svg/360px-Selenium_logo.svg.png" />
+          <img width={55} src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png" />
+          <img width={90} src="https://www.datocms-assets.com/45470/1631026680-logo-react-native.png" />
+          <img width={80} src="https://infiniteblogs.blob.core.windows.net/medias/f486812a-1c7f-4802-ab9d-4c709f0f1bb7_0.png" />
+          <img width={50} src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Vue.js_Logo_2.svg/1200px-Vue.js_Logo_2.svg.png" />
+          <img width={80} src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Node.js_logo.svg/240px-Node.js_logo.svg.png" />
+          <img width={120} src="https://www.fastify.io/images/fastify-logo-menu.d13f8da7a965c800.png" />
+          <img width={90} src="https://res.cloudinary.com/practicaldev/image/fetch/s--m_Ng9MLF--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/i/fppjegg7q1kb2pdzmlvf.png" />
+          <img width={130} src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/64/Expressjs.png/240px-Expressjs.png" />
+          <img width={100} src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/44/Spring_Framework_Logo_2018.svg/240px-Spring_Framework_Logo_2018.svg.png" />
+          
+          <img width={120} src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/66/Nuxt_logo_%282021%29.svg/2560px-Nuxt_logo_%282021%29.svg.png" />
+          <img width={80} src="https://humancoders-formations.s3.amazonaws.com/uploads/course/logo/1827/formation-next-js.png" />
+
+        </div>
+      </div>
+    </div>
+  )
+}
+
+
+
+export default Api ;
--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import Start from './Start'
 import Dots from '../components/Dots'
 import MidOne from './MidOne'
 import Stack from './Stack'
-
+import Api from './API'
 
 export default function Home() {
   return (
@@ -13,6 +13,8 @@ export default function Home() {
       <Dots dotSize={13} width={600} top={404}  gap={20} right={50} color='#5eead4'/>
       <MidOne />
       <Stack />
+      <Dots dotSize={7} width={750} top={1904}  gap={10} right={100} color='#3730a3'/>
+      <Api />
     </main>
   )
 }
